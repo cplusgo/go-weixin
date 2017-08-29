5b0dc6440f113a9b4bbad8bea0f03215c90bad55
aron <aron@arondeMacBook-Pro.local>
2017-08-29 23:58:26 +0800

    添加微信app支付

--- pay.go
+++ pay.go
@@ -16,22 +16,6 @@ import (
  *统一下单接口:https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_1
  */
 
-/*
-<xml>
-   <appid>wx2421b1c4370ec43b</appid>
-   <attach>支付测试</attach>
-   <body>APP支付测试</body>
-   <mch_id>10000100</mch_id>
-   <nonce_str>1add1a30ac87aa2db72f57a2375d8fec</nonce_str>
-   <notify_url>http://wxpay.wxutil.com/pub_v2/pay/notify.v2.php</notify_url>
-   <out_trade_no>1415659990</out_trade_no>
-   <spbill_create_ip>14.23.150.211</spbill_create_ip>
-   <total_fee>1</total_fee>
-   <trade_type>APP</trade_type>
-   <sign>0CB01533B8C1EF103065174F50BCA001</sign>
-</xml>
-*/
-
 type WxPaymentSigned struct {
 	appId      string
 	appKey     string
@@ -127,9 +111,6 @@ func (this *WxPaymentSigned) createPresign() map[string]string {
 	return presignData
 }
 
-/*
- *在统一下单接口执行完毕之后会返回一个关键的数据,prepayid
- */
 func (this *WxPaymentSigned) unifiedorder() (string, error) {
 	presignData := this.createPresign()
 	params := sortURLParams(presignData)
